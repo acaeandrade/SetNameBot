09188cccd290126536c517431a239a524d74fe6d
DanielVds12 <DanielVds12@outlook.com>
2022-01-26 10:33:51 -0300

    Criação de autenticação

---
+++ Autenticação.py
@@ -0,0 +1,15 @@
+from charset_normalizer import api
+import tweepy
+
+def Autenticacao():
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
+
+    #Login a partir das chaves
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    #Declaração para chamar a api sem espamar
+    Api = tweepy.API(auth, wait_on_rate_limit=True)
